8734d2596ca584d1b7b192ab28e6945dd0ed00e8
ishu9805 <abhishek47903@gmail.com>
2024-06-17 04:57:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ MONGO_URI = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
 GROUP_ID = -1002212863321  # Target group ID
 
 # Initialize Pyrogram Client
-api_id = YOUR_API_ID
-api_hash = 'YOUR_API_HASH'
+api_id = 26692918
+api_hash = '2b239375e141e882a33b59820ce827be'
 bot_token = 'BQE--bQAjqHoV0hhvb27cizIgWfl0kHrwKwmGGHyZCP8D65FYLSLm993AOCg5G-xuoCHWPv32qaZfndeeKKo62IpOQc1Wv7Xj7ga2DAYq94D05JdL5pwk5plwdCXQdcBIFFlPIIigEN3ky57nJq_8k6d9qWQSC0m5NqX5gIEbMUfKzspp27zqdXy1WAr_D-Ykxi27CEWAEI9YHqWQe9Ox2OLIbuIzAM6Gzampsub4JisFCxewwE1BUA7COgc4Vvf6zV98AYKd167UPqVhwZEuAF2DJUbNDTw_NFEksUh5Y5oCdRtv4axpdJFpdZ0cocfl-zNkKLDt9oVfQA1Brh9oIzZhBgIkAAAAAF09l8AAA'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, session_string=bot_token)
