5dabdc6ba9b80f7cc203072c87ef115951106633
ishu9805 <abhishek47903@gmail.com>
2024-06-17 11:13:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,10 +55,10 @@ async def handle_photo_message(client, message):
         awaited_message = message
 
 # Event handler to process awaited message reply in groups
-@app.on_message(filters.group & filters.user(572621020) & filters.reply)
+@app.on_message(filters.group & filters.user(572621020))
 async def process_awaited_message(client, message):
     global awaited_message
-    if awaited_message and awaited_message.message_id == message.reply_to_message.message_id:
+    if awaited_message:
         file_unique_id = awaited_message.photo.file_unique_id
         chat_id = awaited_message.chat.id
 
