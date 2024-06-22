62f7c0ff8f11e3ae1c422aa8bda27cd21098afa3
ishu9805 <abhishek47903@gmail.com>
2024-06-22 11:44:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,32 +50,22 @@ def send_guess_message():
     for chat_id in HEXAMON:
         app.send_message(chat_id, "/guess")
 
-@app.on_message(filters.chat(HEXAMONS) & filters.user([572621020]))
+@app.on_message(filters.chat(HEXAMON) & filters.user([572621020]) & filters.photo)
 def get_image_details(client, message):
-    """Handle replies to image messages with the specific caption to fetch details."""
+    file_unique_id = message.photo.file_unique_id
+    image_data = images_collection.find_one({"file_unique_id": file_unique_id})
 
-    if message.caption and "that" in message.caption:
-        file_unique_id = message.photo.file_unique_id
-        image_data = images_collection.find_one({"file_unique_id": file_unique_id})
-
-        if not image_data:
-            logging.info("Image data not found in the database.")
-            if message.reply_to_message and "The pokemon was" in message.reply_to_message.text:
-                character_name = message.reply_to_message.text.split("The pokemon was ")[1]
-                chat_id = -1002048925723
-                anime_name = "Pokemon"
-                response_text = f"Character Name: {character_name}\nAnime Name: {anime_name}"
-                client.send_message(chat_id, response_text)
-            return
-
+    if not image_data:
+        logging.info("Image data not found in the database.")
+        if message.reply_to_message and "The pokemon was" in message.reply_to_message.text:
+            pokemon_name = message.reply_to_message.text.split("The pokemon was ")[1]
+            chat_id = -1002048925723
+            client.send_photo(chat_id, message.photo.file_id, caption=f"The pokemon was {pokemon_name}")
+    else:
         character_name = image_data.get("character_name")
-
         response_text = f"{character_name}"
         time.sleep(2)
         client.send_message(chat_id=message.chat.id, text=response_text)
-    else:
-        logging.info("Caption does not contain the required text.")
-
 
 def schedule_guess_message():
     schedule.every(10).minutes.do(send_guess_message)  # Send /guess message every 1 hour
